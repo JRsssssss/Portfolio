296b9c8a764dbbce7af5c7173546dfb625b3db56
Jirapat Ruetrakul <65011313@kmitl.ac.th>
2023-12-24 00:19:50 +0700

    adad

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,7 +4,7 @@ function App()
 {
   return (
     <>
-      <h1 className="text-4xl font-bold underline text-red-500">
+      <h1 className="text-4xl font-bold underline text-red-500 animate-rotate">
         Hello world!
       </h1>
     </>
