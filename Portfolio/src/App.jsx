9cbf7a87e6877106ce9623fcc6abc6d59b533099
JRsssssss <tackelzaza@gmail.com>
2023-12-25 01:31:55 +0700

    try image but it's not appear in the screen

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-
+import React from 'react';
+import './App.css';
+import earth from './assets/earth.jpg';
 function App() 
 {
   return (
     <>
-      <h1 className="text-4xl font-bold underline text-red-500 rotate-and-updown">
-        Hello world!
-      </h1>
+      <div className="bg-black flex justify-center items-center h-[500px]">
+        <h1 className="text-4xl font-bold underline text-red-500 rotate-animation">
+          <img src="{earth}" alt="wow" />
+        </h1>
+      </div>
+     
     </>
   )
 }
