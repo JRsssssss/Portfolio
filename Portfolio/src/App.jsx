3351d3b8ef114ab1397c9ceae8661bfd7e15bf0a
JRsssssss <tackelzaza@gmail.com>
2023-12-26 01:56:00 +0700

    wow

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import earth from './assets/earth.jpg';
 function App() 
 {
   return (
     <>
       <div className="bg-black flex justify-center items-center h-[500px]">
         <h1 className="text-4xl font-bold underline text-red-500 rotate-animation">
-          <img src="{earth}" alt="wow" />
+          Hello World!
         </h1>
       </div>
      
