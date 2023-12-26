c64ba08f4c3cc7fe4ffb0d5bf13271ea8b340537
JRsssssss <tackelzaza@gmail.com>
2023-12-26 15:14:05 +0700

    adadad

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import earth from "../src/assets/earth.jpg";
 function App() 
 {
   return (
     <>
       <div className="bg-black flex justify-center items-center h-[500px]">
         <h1 className="text-4xl font-bold underline text-red-500 rotate-animation">
-          Hello World!
+          <img src={earth} alt="wow" className="w-[200px] h-[200px]"/>
         </h1>
       </div>
      
