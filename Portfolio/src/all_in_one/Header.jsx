b5d73527c8bae8c8e3d43205c2b27777b6ef544d
Jirapat Ruetrakul <65011313@kmitl.ac.th>
2023-12-28 00:18:27 +0700

    fix screen

--- Portfolio/src/all_in_one/Header.jsx
+++ Portfolio/src/all_in_one/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Header = () =>{
     return(
         <>
-            <div className="flex justify-between bg-#2a2a2a w-screen h-[200px] text-[#c0c0c0]">
-                <div className="mr-auto ml-8">
+            <div className="flex justify-between bg-#2a2a2a w-full h-[200px] text-[#c0c0c0]">
+                <div>
                     <a href="">About me</a>
                 </div>
-                <div className="ml-auto mr-8">
+                <div>
                     <a href="">Contact me</a>
                 </div>
             </div>
