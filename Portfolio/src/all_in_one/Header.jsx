f6fff27177b9a44fea093037f582ae4caf11fe8e
JRsssssss <tackelzaza@gmail.com>
2023-12-27 15:52:58 +0700

    add nav bar and fix some properties

--- Portfolio/src/all_in_one/Header.jsx
+++ Portfolio/src/all_in_one/Header.jsx
@@ -3,9 +3,15 @@ import React from "react";
 export const Header = () =>{
     return(
         <>
-            <div className="bg-white w-screen h-[200px]">
-                <h1>Hello this is Header</h1>
+            <div className="flex justify-between bg-#2a2a2a w-screen h-[200px] text-[#c0c0c0]">
+                <div className="mr-auto ml-8">
+                    <a href="">About me</a>
+                </div>
+                <div className="ml-auto mr-8">
+                    <a href="">Contact me</a>
+                </div>
             </div>
+            
         </>
     )
 }
