5f1450437181469e838ae889918608803f226cc6
JRsssssss <tackelzaza@gmail.com>
2024-01-03 01:54:47 +0700

    change the picture and add animation around the picture but shit maybe it's not good

--- Portfolio/src/all_in_one/Header.jsx
+++ Portfolio/src/all_in_one/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Header = () =>{
     return(
         <>
-            <div className="flex justify-center gap-10 bg-#2a2a2a w-full h-[200px] text-[#c0c0c0] font-baskerville">
-                <div className="text-2xl mt-6">
+            <div className="flex justify-left gap-10 bg-#2a2a2a h-[100px] text-[#c0c0c0] font-baskerville mt-6 ml-10">
+                <div className="text-2xl">
                     <a href="">About me</a>
                 </div>
-                <div className="text-2xl mt-6">
+                <div className="text-2xl">
                     <a href="">Contact me</a>
                 </div>
                 
--- Portfolio/src/all_in_one/Name_Image.jsx
+++ Portfolio/src/all_in_one/Name_Image.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import earth from "../assets/earth.jpg"; // Ensure this path is correct
+import earth from "../assets/Prem.jpg";
+import './Name_image.css'
 
 export const Name_Image = () => {
     return (
-        <div className="flex justify-center items-center space-x-20 h-screen w-full text-[#c0c0c0] font-baskerville">  
-            <div>
-                <h1 className="text-9xl">
+        <div className="flex justify-center items-center mt-[120px] space-x-20 text-[#fff8f8] font-baskerville">  
+            <div className="-mt-1">
+                <h1 className="text-9xl text-shadow">
                     Hello
                 </h1>
-                <p>
-                    I'm Prem(Jirapat Ruetrakul)
+                <p className="">
+                    I'm Prem (Jirapat Ruetrakul) <br />
+                    I'm software engineering student of KMITL <br />
                 </p>
             </div>
-            <div className="text-4xl font-bold underline text-red-500 rotate-animation">
-                <img src={earth} alt="Earth" className="w-[200px] h-[200px]" />
+            <div className="text-4xl font-bold underline text-red-500">
+                <img src={earth} alt="Earth" className="object-cover w-[300px] h-[300px] rounded-full ml-5" />
             </div>
+            <div className="circle"></div>
+            <div className="circle_2"></div>
+            <div className="circle_3"></div>
         </div>
         
     );
