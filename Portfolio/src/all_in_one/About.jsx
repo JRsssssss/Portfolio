c3e454df6b575d72888dcdc6b8ac2b9f46e7c46f
JRsssssss <tackelzaza@gmail.com>
2024-01-07 04:03:30 +0700

    add effect to about part

--- Portfolio/src/all_in_one/About.jsx
+++ Portfolio/src/all_in_one/About.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './About.css'
-export const About = () =>{
-    return(
-        <div id="about_part" className='flex justify-center items-center gap-20 h-[300px] text-[#fff8f8] font-baskerville border'>
-            <div className='about_me_text border'>
+import React, { useState, useEffect } from 'react';
+import './About.css';
+
+export const About = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        const aboutPart = document.getElementById('about_part');
+        const topPosition = aboutPart.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if (!isVisible && topPosition < screenPosition) {
+            setIsVisible(true);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isVisible]);
+
+    return (
+        <div id="about_part" className={`flex justify-center items-center h-[300px] text-[#fff8f8] font-baskerville ${isVisible ? 'show' : 'hide'}`}>
+            <div className='about_me_text'>
                 <h1>About me</h1>
             </div>
-            <div className='about_me_paragraph border'>
+            <div className='about_me_paragraph'>
                 <p>
                     Hello! My name is Jirapat Ruetrakul, but you can call me Prem. I am currently a passionate second-year student at KMITL University,
                     where my journey in the realm of technology and innovation is in full swing. With a keen interest in the interconnected world of
@@ -14,5 +32,5 @@ export const About = () =>{
                 </p>
             </div>
         </div>
-    )
-}
+    );
+};
