4ea4dfb731ba6fc3d7bf04a78347622e5f9b5921
JRsssssss <tackelzaza@gmail.com>
2024-01-07 02:39:54 +0700

    fixed the about me part to become responsive

--- Portfolio/src/all_in_one/About.jsx
+++ Portfolio/src/all_in_one/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './About.css'
 export const About = () =>{
     return(
-        <div id="about_part" className='flex flex-row justify-center gap-20 mt-[400px] h-[300px] text-[#fff8f8] font-baskerville border'>
-            <div className='flex items-center text-6xl border'>
+        <div id="about_part" className='flex justify-center items-center gap-20 h-[300px] text-[#fff8f8] font-baskerville border'>
+            <div className='about_me_text border'>
                 <h1>About me</h1>
             </div>
-            <div className='flex items-center w-[750px] border indent-16 text-2xl'>
+            <div className='about_me_paragraph border'>
                 <p>
                     Hello! My name is Jirapat Ruetrakul, but you can call me Prem. I am currently a passionate second-year student at KMITL University,
                     where my journey in the realm of technology and innovation is in full swing. With a keen interest in the interconnected world of
