feeb33510e3348d502bb2fb731b323762e81c8f0
JRsssssss <tackelzaza@gmail.com>
2024-01-07 19:44:47 +0700

    adadadad

--- Portfolio/src/Combine.jsx
+++ Portfolio/src/Combine.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Name_Image} from './all_in_one/Name_Image';
 import {Header} from './all_in_one/Header';
 import {About} from './all_in_one/About';   
-
+import { Experience } from './all_in_one/Experience';
 const Combinecomponent = () => {
     return(
         <div>
             <Header/>
             <Name_Image/>
             <About/>
+            <Experience/>
         </div>
     )
 }
--- Portfolio/src/all_in_one/Name_Image.jsx
+++ Portfolio/src/all_in_one/Name_Image.jsx
@@ -1,26 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import earth from "../assets/Prem.jpg";
-import './Name_image.css'
+import './Name_image.css';
 
 export const Name_Image = () => {
     return (
-        <div className="flex justify-center items-center mt-[120px] space-x-10 text-[#fff8f8] font-baskerville border">  
+        <div className="flex justify-center items-center mt-[120px] space-x-10 text-[#fff8f8] font-baskerville">  
             <div className="-mt-1 items-center">
-                <h1 className="hello_text border text-center">
-                    Hello
-                </h1>
-                <p className="short_intro border">
+                <h1 className="hello_text text-center">Hello</h1>
+                <p className="short_intro">
                     I'm Prem (Jirapat Ruetrakul) <br />
-                    I'm software engineering student of KMITL <br />
+                    I'm a software engineering student at KMITL <br />
                 </p>
             </div>
-            <div className="text-4xl font-bold underline text-red-500 border">
+            <div className="text-4xl font-bold underline text-red-500">
                 <img src={earth} alt="Earth" className="my_img ml-5" />
             </div>
             <div className="circle"></div>
             <div className="circle_2"></div>
             <div className="circle_3"></div>
         </div>
-        
     );
 };
