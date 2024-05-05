a29582c3987ba56b66258161aadfedd628cc7827
JRsssssss <tackelzaza@gmail.com>
2024-05-06 00:04:01 +0700

    fix

--- Portfolio/src/all_in_one/Project.jsx
+++ Portfolio/src/all_in_one/Project.jsx
@@ -14,7 +14,7 @@ export const Project = () => {
             </div>
             <div id = 'project_container'className='flex justify-center gap-[100px] mt-[200px]'>
                 <div className='first_project gap-[200px]'>
-                    <div>
+                    {/* <div>
                         <h2>Software Engneering KMITL Website</h2>
                         <p className='text_paragraph'>Link: <a id= 'first_link'href="" target='blank' className='link_to_website'>SE KMITL Website</a></p>
                     </div>
@@ -22,7 +22,11 @@ export const Project = () => {
                         <img src= {Home1} alt="home page of SE website project" className='img_first_project'/>
                         <img src= {About1} alt="about pagee of SE website project" className='img_first_project'/>
                         <img src= {Program1} alt="program pagee of SE website project" className='img_first_project'/>
-                    </div> 
+                    </div>  */}
+                    <div>
+                        On process...
+                    </div>
+
                 </div>
             </div>
         </div>
